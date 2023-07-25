882302e1857976846ebe01593156695c32e09637
DuHyeok <asdfgh7537@naver.com>
2023-07-25 15:49:17 +0900

    4일차 학습 - inheritance

---
+++ src/inheritance/Child.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Child extends Parent{
+
+    public Child() {
+        System.out.println("Child Constructor");
+    }
+
+    @Override // 자식클래스가 부모클래스의 메소드를 수정하기 위해 사용, 부모 클래스에 있는 메소드만 사용가능
+              // 그렇지 않을경우 추가로 생성됨
+    public void printName() {
+//        super.printName();
+        System.out.println("Child Print"); // 바뀌어서 출력됨
+        super.printName();
+    }
+}
---
+++ src/inheritance/Parent.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Parent { // 부모 클래스 생성
+
+    public Parent() { // 부모 클래스의 생성자
+        System.out.println("Parent constructor");
+    }
+
+    public void printName() { // 부모 클래스의 메소드
+        System.out.println("Parent print");
+    }
+    
+}
